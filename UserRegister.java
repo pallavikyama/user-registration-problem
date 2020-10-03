b6ddab7824a3c1909e77f865d81ee4be2a6e8e86
pallavikyama <kyamap98@gmail.com>
2020-10-03 12:19:51 +0530

    Added:User last name is added

--- UserRegister.java
+++ UserRegister.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class UserRegister {
 	String firstName;
+	String lastName;
 	boolean x;
 	static Scanner input = new Scanner(System.in);
 
@@ -17,11 +18,30 @@ public class UserRegister {
 			String formula1 = "^[A-Z][A-Za-z]{2,}$";
 			Pattern code = Pattern.compile(formula1);
 			Matcher check = code.matcher(firstName);
+			if (check.matches())
+				x = true;
+			else {
+				System.out.println("First Name entered is not following the pattern.\nPlease try again.");
+				x = false;
+			}
+		} while (!x);
+		userLastName();
+	}
+
+	public void userLastName() {
+		do {
+			// LAST NAME
+			System.out.println(firstName + ", enter your last name: ");
+			lastName = input.next();
+			String formula2 = "^[A-Z][A-Za-z]{2,}$";
+			Pattern code = Pattern.compile(formula2);
+			Matcher check = code.matcher(lastName);
 			if (check.matches()) {
 				x = true;
-				System.out.println("Welcome " + firstName + "!");
+				System.out.println("Welcome " + firstName + " " + lastName + "!");
 			} else {
-				System.out.println("First Name entered is not following the pattern.\nPlease try again.");
+				System.out.println(
+						firstName + ", your last Name entered is not following the pattern.\nPlease try again.");
 				x = false;
 			}
 		} while (!x);
